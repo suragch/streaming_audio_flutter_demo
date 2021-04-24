31fd693aec83c9747646e8155498375daa27aaf7
suragch <studymongolian@gmail.com>
2021-04-24 18:21:52 +0800

    initial commit

---
+++ android/app/src/main/java/dev/suragch/streaming_audio_flutter_demo/MainActivity.java
@@ -0,0 +1,6 @@
+package dev.suragch.streaming_audio_flutter_demo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'page_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final PageManager _pageManager;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageManager = PageManager();
+  }
+
+  @override
+  void dispose() {
+    _pageManager.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Spacer(),
+              ValueListenableBuilder<ProgressBarState>(
+                valueListenable: _pageManager.progressNotifier,
+                builder: (_, value, __) {
+                  return ProgressBar(
+                    progress: value.current,
+                    buffered: value.buffered,
+                    total: value.total,
+                    onSeek: _pageManager.seek,
+                  );
+                },
+              ),
+              ValueListenableBuilder<ButtonState>(
+                valueListenable: _pageManager.buttonNotifier,
+                builder: (_, value, __) {
+                  switch (value) {
+                    case ButtonState.loading:
+                      return Container(
+                        margin: EdgeInsets.all(8.0),
+                        width: 32.0,
+                        height: 32.0,
+                        child: CircularProgressIndicator(),
+                      );
+                    case ButtonState.paused:
+                      return IconButton(
+                        icon: Icon(Icons.play_arrow),
+                        iconSize: 32.0,
+                        onPressed: _pageManager.play,
+                      );
+                    case ButtonState.playing:
+                      return IconButton(
+                        icon: Icon(Icons.pause),
+                        iconSize: 32.0,
+                        onPressed: _pageManager.pause,
+                      );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
